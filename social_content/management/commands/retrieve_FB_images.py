85d7694a48024cc6275224d181d611dd9de243d6
guillecoro <gcoro@devartis.com>
2015-03-30 12:53:22 -0300

    management task para recuperar imagenes de facebook

---
+++ social_content/management/commands/retrieve_FB_images.py
@@ -0,0 +1,22 @@
+from django.core.management.base import BaseCommand
+from social_content.models import FacebookContent
+import urllib
+from django.core.files.base import ContentFile
+
+
+class Command(BaseCommand):
+
+    def handle(self, *args, **options):
+
+        facebook_contents = FacebookContent.objects.all()
+
+        for content in facebook_contents:
+            if content.image_url:
+                IMAGE_URL = content.image_url
+                response = urllib.urlopen(IMAGE_URL)
+                file_name = "%s.jpg" % content.identifier
+                content.image.save(file_name, ContentFile(response.read()), save=False)
+                content.published = True
+                content.save()
+
+__author__ = 'guillermo'
